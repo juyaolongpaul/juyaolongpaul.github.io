f88f4379193675f7168520df994d915bc0a5ec2f
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2025-08-21 08:59:41 +0000

    Deploying to gh-pages from @ juyaolongpaul/juyaolongpaul.github.io@ee87c5ab4b7b2268eddba4bc6e55ecbb2450b410 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "Below is my brief CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
