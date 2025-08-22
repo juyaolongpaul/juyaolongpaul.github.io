9b8819e996a6ac5bdaa61fb6160b756500a9fb6d
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2025-08-22 07:47:42 +0000

    Deploying to gh-pages from @ juyaolongpaul/juyaolongpaul.github.io@edbb6359da11eebe37cf53d74b499b8acb8645d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of my projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
